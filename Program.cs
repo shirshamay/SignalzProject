5faac313f90e9a7237bb7375129f077ea55a7ed4
ShirShamay <shirshamay19454@outlook.com>
2026-05-14 13:37:25 +0300

    First working version: C# API with jQuery

---
+++ Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+ options.AddDefaultPolicy(policy =>
+  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+app.UseCors();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "SIGNAL_BUY", "SIGNAL_SELL"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
