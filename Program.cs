91c0b10098828444810fbbfe53c6da93a523a597
ShirShamay <shirshamay19454@outlook.com>
2026-05-14 14:11:57 +0300

    Full Stack Signal App: .NET API + jQuery with Price and CORS

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options =>
- options.AddDefaultPolicy(policy =>
-  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
 app.UseCors();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -16,19 +18,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "SIGNAL_BUY", "SIGNAL_SELL"
-};
+var summaries = new[] { "SIGNALZ_BUY", "SIGNALZ_SELL" };
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+            summaries[Random.Shared.Next(summaries.Length)],
+            (decimal)Random.Shared.Next(50000, 60000) + (decimal)Random.Shared.NextDouble()
         ))
         .ToArray();
     return forecast;
@@ -36,8 +36,4 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, decimal Price);
